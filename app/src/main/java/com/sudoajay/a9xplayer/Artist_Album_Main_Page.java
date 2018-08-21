164f496dbc86f28ded8663a05075c670c81bd065
sudoajay <sudoajay@gmail.com>
2018-08-21 06:58:51 +0530

    new commit

---
+++ app/src/main/java/com/sudoajay/a9xplayer/Artist_Album_Main_Page.java
@@ -0,0 +1,14 @@
+package com.sudoajay.a9xplayer;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Artist_Album_Main_Page extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist__album__main__page);
+    }
+
+}
